8c29e372dc7178c01515df11f7b8b8fcfa92f62c
michaelmedvinskiy <michael.medvinskiy@gmail.com>
2026-06-05 14:59:47 +0200

    run-tests.bat only for net8.0

--- run-tests.bat
+++ run-tests.bat
@@ -1,12 +1,12 @@
 @echo off
-REM Run all tests in the "test" folder
+REM Run all tests in the "test" folder targeting .NET 8.0
 setlocal
 
 set SCRIPT_DIR=%~dp0
 
 for /R "%SCRIPT_DIR%test" %%f in (*.csproj) do (
     echo Running tests for %%f
-    dotnet test "%%f" || exit /b 1
+    dotnet test "%%f" --framework net8.0 || exit /b 1
 )
 
 echo All tests passed.
